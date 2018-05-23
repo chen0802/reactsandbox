b5947d5e4d5c60d77bcaf1d1858642cbe8031a61
chen0802 <chen0802@gmail.com>
2018-05-22 23:29:23 -0400

    Added 52 common conditions plain buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { ColorButton} from './ColorButton'
 import { Clock, Welcome, Toggle } from './Clock'
 import { BitcoinPrice } from './BitcoinPrice'
 import LoginControl from './LoginControl'
@@ -38,7 +39,236 @@ class App extends React.Component {
         }
     }
 
+
     render() {
+        const CommonConditions = [
+            {
+                index: 1,
+                display: 'Common cold and influenza',
+                value: 'Common cold and influenza'
+            },   
+            {
+                index: 2,
+                display: 'Cough'
+            },
+            {
+                index: 3,
+                display: 'Jaundice'
+            },
+            {
+                index: 4,
+                display: 'Dizziness and Vertigo'
+            },
+            {
+                index: 5,
+                display: 'Asthma'
+            },
+            {
+                index: 6,
+                display: 'Epigastric Pain'
+            },
+            {
+                index: 7,
+                display: 'Acid reflux'
+            },
+            {
+                index: 8,
+                display: 'Gastric Upset'
+            },
+            {
+                index: 9,
+                display: 'Abdominal pain'
+            },
+            {
+                index: 10,
+                display: 'Gan Ji (Indigestion for Children)'
+            },
+            {
+                index: 11,
+                display: 'Tiredness'
+            },
+            {
+                index: 12,
+                display: 'Hermorrhoid'
+            },
+            {
+                index: 13,
+                display: 'Diarrhoea'
+            },
+            {
+                index: 14,
+                display: 'Constipation'
+            },
+            {
+                index: 15,
+                display: 'Diabetes'
+            },
+            {
+                index: 16,
+                display: 'Painful Obstruction Syndrome (Bi Zheng)'
+            },
+            {
+                index: 17,
+                display: 'Facial Pain'
+            },
+            {
+                index: 18,
+                display: 'Lower Back Pain'
+            },
+            {
+                index: 19,
+                display: 'Neck Pain'
+            },
+            {
+                index: 20,
+                display: "Mian Tan, Facial Paralysis, Bell's Palsy"
+            },
+            {
+                index: 21,
+                display: 'Headache'
+            },
+            {
+                index: 22,
+                display: 'Wind Stroke'
+            },
+            {
+                index: 23,
+                display: 'Atrophy Syndrome'
+            },
+            {
+                index: 24,
+                display: 'Sprains'
+            },
+            {
+                index: 25,
+                display: 'Sudden Deafness'
+            },
+            {
+                index: 26,
+                display: 'Er Ming, Ringing in the Ear/Tinnitus'
+            },
+            {
+                index: 27,
+                display: 'Ear Pain'
+            },
+            {
+                index: 28,
+                display: 'Myopia'
+            },
+            {
+                index: 29,
+                display: 'Bi Yan, Sinus Infection'
+            },
+            {
+                index: 30,
+                display: 'Throat Pain'
+            },
+            {
+                index: 31,
+                display: 'Eczema'
+            },
+            {
+                index: 32,
+                display: 'Acne'
+            },
+            {
+                index: 33,
+                display: 'Shingles'
+            },
+            {
+                index: 34,
+                display: 'Impotence'
+            },
+            {
+                index: 35,
+                display: 'Infertility'
+            },
+            {
+                index: 36,
+                display: 'Premenstruel Syndromes'
+            },
+            {
+                index: 37,
+                display: 'Dysmenorrhoea'
+            },
+            {
+                index: 38,
+                display: 'Vaginal Itchiness'
+            },
+            {
+                index: 39,
+                display: 'Irregular Menstruation'
+            },
+            {
+                index: 40,
+                display: 'Morning Sickness'
+            },
+            {
+                index: 41,
+                display: 'Menopause'
+            },
+            {
+                index: 42,
+                display: 'Amenorrhea'
+            },
+            {
+                index: 43,
+                display: 'Abnormal Uterus Bleeding'
+            },
+            {
+                index: 44,
+                display: 'Depression'
+            },
+            {
+                index: 45,
+                display: 'Insomnia'
+            },
+            {
+                index: 46,
+                display: 'Hypochondrial Pain'
+            },
+            {
+                index: 47,
+                display: 'Vomiting'
+            },
+            {
+                index: 48,
+                display: 'Palpitation'
+            },
+            {
+                index: 49,
+                display: 'Edema'
+            },
+            {
+                index: 50,
+                display: 'Urinary Retention'
+            },
+            {
+                index: 51,
+                display: 'Syncope'
+            },
+            {
+                index: 52,
+                display: 'Toothache'
+            }
+        ]
+
+        const commonConditionButtons = CommonConditions.map(cond => {
+            return <ColorButton> {cond.display} </ColorButton>
+        });
+
+        return (
+            <div>
+                <Greeting  name='please select common conditions'/>
+                <br />
+                <div>
+                    {commonConditionButtons}
+                </div>
+            </div>
+        )
+    }
+
+    rendermenu() {
         return (
             <Router>
                 <div>
---
+++ src/components/ColorButton.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+
+class ColorButton extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {isToggleOn: true};
+      this.myRef = React.createRef();
+      // This binding is necessary to make `this` work in the callback
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    handleClick() {
+      this.setState(prevState => ({
+        isToggleOn: !prevState.isToggleOn
+      }));
+    }
+  
+    render() {
+      const children = this.props.children;
+      return (
+        <div class="btn-group mr-2 mb-2" role="group" aria-label={children}>
+            <button type="button" data-toggle="button" aria-pressed="false" class="btn btn-secondary" onClick={this.handleClick}>
+                {children}
+            </button>
+        </div>
+      );
+    }
+}
+
+export {ColorButton}
